2249ece27d4a1c58bd3d65006f2b0317a4293a81
jx <jxaop1@gmail.com>
2025-05-30 23:08:53 -0300

    Add files via upload

--- vscode/assets/app.js
+++ vscode/assets/app.js
@@ -1,11 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const cartaoHtml = document.getElementById('cartao-html');
+    if (cartaoHtml) {
+        cartaoHtml.addEventListener('click', function() {
+            cartaoHtml.classList.toggle('virado');
+            clickSound.currentTime = 0;
+            clickSound.play();
+        });
+    }
+});
+
+const clickSound = new Audio('assets/sounds/click.mp3');
+
 function criaCartao(conteudo, pergunta, resposta) {
-    
-    
-    
+
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-    
+
     cartao.innerHTML = `
         <div class="conteudo">
             <h3>${conteudo}</h3>
@@ -17,5 +28,16 @@ function criaCartao(conteudo, pergunta, resposta) {
             <p>${resposta}</p>
         </div>
 `;
+cartao.addEventListener('click', function() {
+        cartao.classList.toggle('virado');
+        clickSound.currentTime = 0;
+    clickSound.play();
+
+});
+
+
+
+    
+
     container.appendChild(cartao);
 }
